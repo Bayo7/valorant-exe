d21e42ff82b17f53c4cceb65d0327058d2cddd09
Bayo7 <63423641+Bayo7@users.noreply.github.com>
2020-04-09 22:37:22 +0200

    .py file

---
+++ sos.py
@@ -0,0 +1,22 @@
+import time
+from pynput.keyboard import Key, Controller
+
+keyboard = Controller()
+
+time.sleep(15)
+
+while 1 == 1:
+    keyboard.press('l')
+    keyboard.release('l')
+    keyboard.press('o')
+    keyboard.release('o')
+    keyboard.press('l')
+    keyboard.release('l')
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+    print('Done!')
+    c=180
+    while c != 0:
+        print(c)
+        c-=1
+        time.sleep(1)
